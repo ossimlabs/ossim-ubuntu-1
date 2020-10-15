260da8a8e48d29657dbfedf28c8529b1179af18d
Michael Kresslein 1 <michael.kresslein@maxar.com>
2020-10-15 15:01:51 -0400

    include more libraries

--- runtime/build-docker.sh
+++ runtime/build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker build -t ossim-runtime-ubuntu .
-docker run -it --rm  -v $PWD/dist:/dist ossim-runtime-ubuntu tar -C /usr/local -czvf /dist/ossim-runtime-ubuntu-18.04.tgz .
+docker run -it --rm  -v $PWD/dist:/dist ossim-runtime-ubuntu tar czvf /dist/ossim-runtime-ubuntu-18.04.tgz -C /usr/local . -C /usr lib/
